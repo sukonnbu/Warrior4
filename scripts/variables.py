1b321328bf32c664dc202709bb7a4c549376ee80
sukonnbu <choijiwan1206@gmail.com>
2022-12-06 16:53:23 +0900

    1.1.4

---
+++ scripts/variables.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.font.init()
+
+
+screen_size = WIDTH, HEIGHT = 1000, 600
+BG_COLOR = (255, 255, 255)
+
+title_font = pygame.font.SysFont('freesanbold', 150, True)
+def_font = pygame.font.SysFont('freesanbold.ttf', 75)
